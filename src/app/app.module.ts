3330c37ba32e09c73d46425791ccb3736caf52d9
Thomas Nix <tnix2@uncc.edu>
2017-10-07 15:38:56 -0400

    Component structure set up.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SelectionComponent } from './selection/selection.component';
+import { MainContentComponent } from './main-content/main-content.component';
+import { ProjectComponent } from './main-content/project/project.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SelectionComponent,
+    MainContentComponent,
+    ProjectComponent
   ],
   imports: [
     BrowserModule
